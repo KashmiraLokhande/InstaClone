f59f16045ee40de12fb3864772e0e50eb8166f0b
Kashmira Lokhande <kashmiralokhande1198@gmail.com>
2022-09-15 16:41:56 -0700

    complete project

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,42 @@
-const bodyParser = require('body-parser');
-const { Console } = require('console');
-const express = require('express');
-const { default: mongoose } = require('mongoose');
-const app = express();
-const mangoose = require('mongoose');
-const postRoutes = require('./routes/posts')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
 
 // kash
 // aNJsoKwYhAwXRCui
 
-mongoose.connect("mongodb+srv://kash:aNJsoKwYhAwXRCui@cluster0.k4idf.mongodb.net/node-angular?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://kash:aNJsoKwYhAwXRCui@cluster0.k4idf.mongodb.net/node-angular")
   .then(() => {
-      console.log('Connected to database!')
+    console.log("Connected to database!");
   })
-  .catch(() =>{
-    console.log('Connection failed!')
+  .catch(() => {
+    console.log("Connection failed!");
   });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
+
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Origin', '*'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-app.use('/api/posts', postRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
-
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,59 +1,20 @@
 const express = require("express");
 
+const PostController = require("../controllers/posts");
+
+const checkAuth = require("../middleware/check-auth");
+const extractFile = require("../middleware/file");
+
 const router = express.Router();
-const Post = require('../models/post')
 
-router.post('',(req,res,next) =>{
-    const post = Post({
-      title: req.body.title,
-      content: req.body.content
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: 'Post added Successfully!',
-        postId: createdPost._id
-      });
-    });
-    
-  });
-  
-  router.get('',(req,res,next) => {
-    Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message:'Posts fetched successfully',
-        posts: documents
-      });
-    });
-  });
+router.post("", checkAuth, extractFile, PostController.createPost);
 
-  router.put("/:id",(req, res, next) => {
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content
-    });
-    Post.updateOne({_id: req.params.id}, post).then(result => {
-      console.log(result);
-      res.status(200).json({message: 'Update successful!'})
-    });
-  });
-  
-  router.get("/:id", (req,res,next) => {
-    Post.findById(req.params.id).then(post => {
-      if(post){
-        res.status(200).json(post);
-      }
-      else{
-        res.status(404).json({message: 'Post not found!'});
-      }
-    });
-  });
+router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
-  router.delete("/:id" ,(req,res,next) => {
-    Post.deleteOne({_id: req.params.id}) .then(result => {
-      res.status(200).json({ message:"post Deleted" });    
-    });
-  });
-  
-  module.exports = router;
+router.get("", PostController.getPosts);
+
+router.get("/:id", PostController.getPost);
+
+router.delete("/:id", checkAuth, PostController.deletePost);
+
+module.exports = router;
